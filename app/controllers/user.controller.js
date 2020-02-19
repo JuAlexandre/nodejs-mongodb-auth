b596d67cfd56e44ced993e86bc600ae8edcd4204
Julien <julien.alexandre26@gmail.com>
2020-02-19 14:51:56 +0100

    Creation of the controller method allowing to find a user by id

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -36,5 +36,21 @@ module.exports = {
     } catch (error) {
       return res.status(500).json({ message: error.message });
     }
+  },
+
+  findById: async (req, res) => {
+    if (isNaN(req.params.id)) {
+      return res.status(400).json({ message: 'The supplied parameter is not a number...' });
+    }
+
+    try {
+      const user = await User.findById(req.params.id);
+      return res.status(200).json(user);
+    } catch (error) {
+      if (error.message === 'not_found') {
+        return res.status(404).json({ message: 'No user found...' });
+      }
+      return res.status(500).json({ message: error.message });
+    }
   }
 };
