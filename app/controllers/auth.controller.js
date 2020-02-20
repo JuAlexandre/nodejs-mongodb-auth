e62373379ca3f8eddc65202f5486ccc99880694a
Julien <julien.alexandre26@gmail.com>
2020-02-20 11:02:26 +0100

    Creation of the sign in method

--- app/controllers/auth.controller.js
+++ app/controllers/auth.controller.js
@@ -1,3 +1,4 @@
+const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
 
 const User = require('../models/user.model');
@@ -13,6 +14,30 @@ module.exports = {
     try {
       const user = await User.create(newUser);
       return res.status(201).json(user);
+    } catch (error) {
+      return res.status(500).json({ message: error.message });
+    }
+  },
+
+  signIn: async (req, res) => {
+    try {
+      const users = await User.findByEmail(req.body.email);
+
+      if (users.length === 0) {
+        return res.status(404).json({ message: 'No user found...' });
+      }
+
+      if (!bcrypt.compareSync(req.body.password, users[0].password)) {
+        return res.status(401).json({ message: 'Invalid Password!' });
+      }
+
+      const token = jwt.sign(
+        { ...users[0] },
+        process.env.SECRET,
+        { expiresIn: 86400 /* 24 hours */ }
+      );
+  
+      return res.status(200).json({ token });
     } catch (error) {
       return res.status(500).json({ message: error.message });
     }
