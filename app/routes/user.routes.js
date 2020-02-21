6dca3072630000c7963204ee1033d897620a4147
Julien <julien.alexandre26@gmail.com>
2020-02-21 08:15:34 +0100

    Creation of the /users/:id/profiles routes to find the profile corresponding to the user

--- app/routes/user.routes.js
+++ app/routes/user.routes.js
@@ -1,8 +1,11 @@
 const express = require('express');
 
 const user = require('../controllers/user.controller');
+const profileRoutes = require('./profile.routes');
 
 const router = express.Router();
+
+router.use('/:id', profileRoutes);
 
 router.get('/:id', user.findById);
 
