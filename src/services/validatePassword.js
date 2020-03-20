c8208b6ad288ccf9f5d8ba93645c4281301b1a3f
Gautier Gueri <gautiergueri@gmail.com>
2020-03-20 14:13:54 +0100

    fix pattern password and comment

--- src/services/validatePassword.js
+++ src/services/validatePassword.js
@@ -1,11 +1,11 @@
 const validatePassword = password => {
   /* Password must contain at least 8 character,
       one digit,
-      one special character,
-      one upper case character,
       one lower case character
+      one upper case character,
+      one special character,
   */
-  const pattern = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z])(?=.*[!@#\$%\^&\*]).{8,}$/;
+  const pattern = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#\$%\^&\*]).{8,}$/;
   return pattern.test(password);
 };
 
